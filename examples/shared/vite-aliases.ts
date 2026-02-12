c791edba7158cd9be611d2de29064cd1d2717197
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2026-02-13 02:26:31 +0700

    Fix example build fail due to vite type

--- examples/shared/vite-aliases.ts
+++ examples/shared/vite-aliases.ts
@@ -1,7 +1,11 @@
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import type { Alias } from "vite";
+
+interface Alias {
+  find: string;
+  replacement: string;
+}
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const packagesDir = path.resolve(__dirname, "../../packages");
